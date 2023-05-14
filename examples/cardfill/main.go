40925598122679afb1ab424f2bf3c8093e253a26
Yarden Sod-Moriah <yardnsm@gmail.com>
2023-05-14 23:44:24 +0300

    Refine the cardfill example

--- examples/cardfill/main.go
+++ examples/cardfill/main.go
@@ -1,8 +1,9 @@
-package cardfill
+package main
 
 import (
 	"fmt"
 	"log"
+	"math"
 
 	"github.com/yardnsm/gohever"
 )
@@ -18,21 +19,23 @@ func main() {
 
 	status, err := keva.GetStatus()
 	if err != nil {
-		log.Fatalf("unable to fetch the card status: %v", err)
+		log.Fatalf("unable to fetch the card status: %v\n", err)
 	}
 
 	// Check how much is left to fill up the card
-	if status.RemainingOnCardAmount > status.RemainingMonthlyAmount {
-		log.Fatalf("unable to fill the card: remaining amount exceeding allowed monthly amount")
+	amountToFill := math.Min(status.RemainingOnCardAmount, status.RemainingMonthlyAmount)
+	if amountToFill == 0 {
+		log.Fatalf("unable to fill the card: card is full / you've reached your monthly limit\n")
 	}
+
 
 	estimate, err := status.Estimate(status.RemainingOnCardAmount)
 	if err != nil {
-		log.Fatalf("unable to make estimations: %v", err)
+		log.Fatalf("unable to make estimations: %v\n", err)
 	}
 
 	fmt.Printf(
-		"filling up the card up to %dILS by loading it with %.2fILS (%.2fILS after discount)",
+		"filling up the card up to %dILS by loading it with %.2fILS (%.2fILS after discount)\n",
 		status.MaxOnCardAmount,
 		estimate.Total,
 		estimate.TotalFactored,
@@ -40,17 +43,17 @@ func main() {
 
 	result, err := keva.Load(*status, int32(status.RemainingOnCardAmount))
 	if err != nil {
-		log.Fatalf("unable to perform load request: %v", err)
+		log.Fatalf("unable to perform load request: %v\n", err)
 	}
 
 	switch result.Status {
 	case gohever.StatusSuccess:
-		fmt.Printf("card was loaded successfuly! load number: %s", result.LoadNumber)
+		fmt.Printf("card was loaded successfuly! load number: %s\n", result.LoadNumber)
 		fmt.Printf("raw message from Hever: %s", result.RawMessage)
 	case gohever.StatusNone:
 		fallthrough
 	case gohever.StatusError:
-		fmt.Printf("card load failed")
-		fmt.Printf("raw message from Hever: %s", result.RawMessage)
+		fmt.Printf("card load failed\n")
+		fmt.Printf("raw message from Hever: %s\n", result.RawMessage)
 	}
 }
